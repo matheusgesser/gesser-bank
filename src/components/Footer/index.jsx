cc6cf71fafcd83b60569f7f018000c17de84080e
MatheusGesser <matheusmvg@hotmail.com>
2023-03-30 08:23:17 -0300

    feat: add improved footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 
-import { Wrapper } from './styles'
+import { Wrapper, Logo } from './styles'
+
+import WingIcon from '../../assets/wing-logo.svg'
 import { BsFacebook, BsInstagram, BsLinkedin, BsTwitter, BsYoutube } from 'react-icons/bs'
 
 
@@ -9,16 +11,52 @@ export default function Footer() {
     <>
       <Wrapper>
         <section>
+          {/* LEFT SIDE */}
+          <section>
+            <Logo onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>
+              <img src={WingIcon} />
+              <h1>Gesser Bank</h1>
+            </Logo>
+            <p>Connect with us</p>
+            <aside>
+              <BsFacebook />
+              <BsInstagram />
+              <BsTwitter />
+              <BsYoutube />
+              <BsLinkedin />
+            </aside>
+          </section>
+          {/* RIGHT SIDE */}
+          <section>
+            <section>
+              <h4>Product</h4>
+              <ul>
+                <li>How it works</li>
+                <li>Services</li>
+                <li>Pricing</li>
+              </ul>
+            </section>
+            <section>
+              <h4>Company</h4>
+              <ul>
+                <li>About</li>
+                <li>FAQs</li>
+                <li>Contact</li>
+                <li>Blog</li>
+              </ul>
+            </section>
+            <section>
+              <h4>Utility</h4>
+              <ul>
+                <li>Privacy Policy</li>
+                <li>Terms of Use</li>
+              </ul>
+            </section>
+          </section>
         </section>
         <section>
-          <strong>© 2023 | All Rights Reserved</strong>
-          <aside>
-            <BsFacebook />
-            <BsInstagram />
-            <BsTwitter />
-            <BsYoutube />
-            <BsLinkedin />
-          </aside>
+          <strong>© Gesser Bank 2023 | Made with ❤️ by Matheus</strong>
+
         </section>
       </Wrapper>
     </>
